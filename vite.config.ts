8695b9cc78cf638dc830f5d14caded3ec529b2a1
Iftikar Alam <meiftikaralam@gmail.com>
2025-10-06 16:30:46 +0530

    change to vetfarm

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/care-snap-petbabu/" : "/",
+  base: mode === "production" ? "/vetfarm/" : "/",
   preview: {
     host: "::",
     port: 4173,
